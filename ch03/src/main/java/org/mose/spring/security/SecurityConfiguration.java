d9604e99e41b6483a2c322ed1b65dac198a542ae
wexgundam <wexgundam@163.com>
2017-07-19 23:14:37 +0800

    开启remember me设置

---
+++ ch03/src/main/java/org/mose/spring/security/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package org.mose.spring.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Description:Spring Security的java configuration
+ *
+ * @Author: 靳磊
+ * @Date: 2017/7/19 13:47
+ */
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /**
+     * Description：配置Spring Security
+     * 1.允许任何人访问登录地址（"/loging.html"）
+     * 2.用户认证处理地址("/login")
+     * 3.允许任何人访问静态资源目录（"/assets/**"）
+     * 4.开启对任何地址（"/**"）的访问控制，要求必须句别"ROLE_USER"的角色
+     * 5.开启默认form表单形式的用户登入，访问地址为"/login.jsp"，登录成功后自动跳转到用户前一次的访问地址
+     * 6.开启remember me设置
+     * 7.关闭csrf限制，该功能以后再讲，默认为开启状态<br>
+     *
+     * @param http
+     * @return
+     * @Author: 靳磊
+     * @Date: 2017/7/19 13:47
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/assets/**").permitAll()
+                .antMatchers("/**").hasRole("USER")
+                .and().formLogin().loginPage("/login.jsp").permitAll().loginProcessingUrl("/login")
+                .and().rememberMe()
+                .and().csrf().disable();
+    }
+}
