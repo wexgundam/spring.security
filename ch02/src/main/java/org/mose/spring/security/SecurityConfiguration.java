4434ecb47d82d6a25c9ab319529cc36fb8d4717d
wexgundam <wexgundam@163.com>
2017-07-19 22:24:36 +0800

    自定义用户界面/login.jsp 用户认证处理地址/login

--- ch02/src/main/java/org/mose/spring/security/SecurityConfiguration.java
+++ ch02/src/main/java/org/mose/spring/security/SecurityConfiguration.java
@@ -15,14 +15,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     /**
      * Description：配置Spring Security
      * 1.允许任何人访问登录地址（"/loging.html"）
-     * 2.允许任何人访问静态资源目录（"/assets/**"）
-     * 3.开启对任何地址（"/**"）的访问控制，要求必须句别"ROLE_USER"的角色
-     * 4.开启默认form表单形式的用户登入，访问地址为"/login.html"，登录成功后自动跳转到用户前一次的访问地址
-     * 5.关闭csrf限制，该功能以后再讲，默认为开启状态<br>
+     * 2.用户认证处理地址("/login")
+     * 3.允许任何人访问静态资源目录（"/assets/**"）
+     * 4.开启对任何地址（"/**"）的访问控制，要求必须句别"ROLE_USER"的角色
+     * 5.开启默认form表单形式的用户登入，访问地址为"/login.jsp"，登录成功后自动跳转到用户前一次的访问地址
+     * 6.关闭csrf限制，该功能以后再讲，默认为开启状态<br>
      *
      * @param http
      * @return
-     *
      * @Author: 靳磊
      * @Date: 2017/7/19 13:47
      */
@@ -30,10 +30,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/login.html").permitAll()
                 .antMatchers("/assets/**").permitAll()
                 .antMatchers("/**").hasRole("USER")
-                .and().formLogin().loginPage("/login.html")
+                .and().formLogin().loginPage("/login.jsp").loginProcessingUrl("/login").permitAll()
                 .and().csrf().disable();
     }
 }
